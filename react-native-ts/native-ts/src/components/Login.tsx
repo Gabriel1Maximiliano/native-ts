ed80d390a5ffee521f89ef89d804619eedff7ab6
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-04-08 09:45:40 -0300

    feature login and logout added

--- react-native-ts/native-ts/src/components/Login.tsx
+++ react-native-ts/native-ts/src/components/Login.tsx
@@ -1,29 +1,141 @@
+import { useEffect, useReducer } from "react";
 
 
+export interface IAuthStateProps {
+
+    isValidating: boolean;
+    token       : string | null;
+    username    : string;
+    name        : string;
+
+}
+
+const initialState : IAuthStateProps = {
+    isValidating: true,
+    token:null,
+    username:'',
+    name:''
+}
+
+type LoginPayload = {
+
+    name    :string;
+    username: string;
+
+}
+
+type AuthActions = 
+|{ type:"[Auth-Reducer]-Logout" }
+|{ type:"[Auth-Reducer]-Login",payload:LoginPayload }
+|{ type:"[Auth-Reducer]-Logout-User", }
+
+
+const authReducer = ( state:IAuthStateProps, action:AuthActions ): IAuthStateProps =>{
+  switch( action.type ){
+
+    case "[Auth-Reducer]-Logout":
+        return{
+            ...state,
+            isValidating:false,
+            token       :null,
+            username    :'',
+            name        :''
+
+        }
+        case "[Auth-Reducer]-Login":
+            return{
+                ...state,
+                token:'abd123',
+                username:action.payload.username,
+                name    :action.payload.name
+    
+            }
+            case "[Auth-Reducer]-Logout-User":
+                return{
+                    ...state,
+                    isValidating:false,
+                    token       :null,
+                    username    :'',
+                    name        :''
+        
+                }
+    default: return state;
+  }
+}
 export const Login = () => {
+
+const [ state, dispath ] = useReducer( authReducer,initialState );
+const { isValidating, token }  = state;
+
+
+const handleLogin = () =>{
+    dispath( { type:"[Auth-Reducer]-Login", payload:{
+        username    :'Loly',
+        name        :'Lola Monasterio'} } )
+}
+const handleLogout = () =>{
+    dispath( { type:"[Auth-Reducer]-Logout-User" })
+}
+
+useEffect(() => {
+ 
+    setTimeout( () => {
+        dispath( { type:"[Auth-Reducer]-Logout" } )
+    },1500 )
+
+  
+}, [])
+
+
   return (
     <div>
         <h1>Login</h1>
-        <div className="alert alert-info" >
+        {
+            isValidating 
+            ?
+             (
+                <div className="alert alert-info" >
             Validando...
         </div>
-        <div className="alert alert-error" >
-            No autenticado
-        </div>
-        <div className="alert alert-success" >
-            No autenticado
-        </div>
+            )
+            :
+            (
+            <>
+            {
+                token ? <div className="alert alert-success" >
+                    Autenticado
 
-        <button
-        className="btn btn-primary"
-        >
-            Login
-        </button>
-        <button
-        className="btn btn-danger"
-        >
-            LogOut
-        </button>
+                    <pre>{ JSON.stringify( state,null,2 ) }</pre>
+                    </div>
+                : <div className="alert alert-danger" >No autenticado</div>
+            }
+               
+                
+            </>
+            )
+        }
+        
+        {
+            token ? (
+                <button
+                className="btn btn-danger"
+                onClick={handleLogout }
+                >
+                    LogOut
+                </button>
+            ):(
+                <button
+                className="btn btn-primary"
+
+                onClick={ handleLogin }
+                >
+                    Login
+                </button>
+            )
+        }
+
+       
+       
     </div>
   )
 }
